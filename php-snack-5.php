40bd4ea9b6e42a956a51500aaa77091be13b179f
Giuseppe <giuseppecantafio@hotmail.it>
2022-05-23 18:51:34 +0200

    snack 5 completato

---
+++ php-snack-5.php
@@ -0,0 +1,74 @@
+<?php 
+//Snack 5
+// Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.
+
+$text = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta aliquid ut reprehenderit iusto aspernatur perspiciatis magni. Quasi, fugit, magni facilis, nemo ratione quis minima fugiat voluptate eligendi quia dolores minus.
+        Deserunt officiis ipsum aspernatur nihil, consequuntur eaque ut dolore minus, suscipit, rerum nam fugit quam commodi soluta aperiam excepturi porro possimus at quia cum quasi ab laudantium? Voluptatibus, magnam corrupti.
+        Sequi nisi minus explicabo a voluptatem iure sit dolore nemo expedita atque? Ab corrupti blanditiis aperiam reiciendis eveniet, illum consectetur corporis et delectus temporibus iure unde amet. Aut, laborum at!
+        Iusto suscipit, ratione quos laborum sint, consequatur saepe impedit neque asperiores non blanditiis magnam nulla at! Quaerat repudiandae quas amet beatae vero soluta fuga necessitatibus possimus, porro, suscipit nesciunt corrupti?
+        Enim, assumenda nesciunt ipsum iusto inventore beatae culpa odit, ducimus itaque alias id minima perferendis velit quidem. Saepe ducimus, modi error fugiat eaque aperiam iusto quisquam nihil pariatur facilis exercitationem.
+        Praesentium, temporibus porro ipsam deserunt, est repudiandae fuga vero quia recusandae exercitationem assumenda unde? Laborum voluptate nihil blanditiis ratione amet officia eius perferendis nemo officiis corrupti? Nostrum rerum officiis nihil.
+        Cumque itaque hic, officiis tempore commodi ratione autem sit cupiditate maiores repudiandae magnam placeat inventore quidem! Eius, voluptas. Dolores fugiat hic tenetur quas voluptates soluta sint ad ipsum inventore nam?
+        Amet, asperiores totam! Tenetur, at assumenda unde nesciunt optio, quasi dignissimos ratione animi quo quis dolor esse, maiores pariatur. Voluptas est eveniet possimus non, cupiditate numquam impedit commodi similique. Repellendus.
+        Natus perferendis modi nisi, ea distinctio recusandae quidem commodi aliquid ipsa asperiores sint explicabo vitae atque corporis animi officiis est in quia sequi? Sed id dolore numquam porro corrupti nobis.
+        Aperiam alias ea quia, possimus laborum adipisci nulla debitis numquam atque vitae placeat odio itaque accusantium. A provident numquam, necessitatibus blanditiis at dolore exercitationem ea accusamus asperiores reprehenderit, ut eveniet?
+        Impedit eaque cum fugiat quo reiciendis obcaecati natus architecto facere, eum quaerat aperiam a tempora accusamus incidunt delectus minima iusto vitae in laboriosam? Omnis magni praesentium voluptate quibusdam earum at.
+        Dolorum a odio voluptate non exercitationem sapiente, perspiciatis ab iste nihil incidunt consectetur deserunt, fugiat doloribus alias amet rem facere sit animi reiciendis hic eveniet architecto. Earum voluptatum non neque.
+        Quae perferendis consectetur molestias voluptates cum tenetur earum, quaerat aspernatur tempore placeat? Temporibus ex iusto dignissimos enim architecto similique obcaecati, assumenda tempore beatae reiciendis amet quos? Accusamus quidem adipisci excepturi.
+        Minima perferendis veniam quae nesciunt fugit autem commodi dolorem, aliquam consequatur, iste atque quos reiciendis molestiae maxime sed delectus. Ut commodi laborum assumenda quidem accusantium consequuntur, aliquam ad debitis rerum.
+        Recusandae, tempora facere fugit aperiam porro ducimus reprehenderit voluptates corporis rerum sit totam officia molestiae incidunt eos esse deleniti ad adipisci atque eum iste est ullam tenetur saepe. Facere, cumque!
+        Labore ea exercitationem facere modi, voluptatum repudiandae quibusdam laboriosam quis eum est sunt molestiae commodi iusto consectetur unde, incidunt illo harum ipsum quam ex fugiat provident nostrum! Vel, dolores rem.
+        Eveniet natus voluptatibus quod dolorem asperiores modi, fuga esse nemo perspiciatis ad labore veniam odit porro eius eaque tempore distinctio delectus hic cupiditate tempora. Reiciendis quis nobis exercitationem dolor soluta.
+        Mollitia optio, ipsa quisquam sed beatae modi molestias consequuntur suscipit ut unde pariatur doloremque nisi quaerat? Debitis omnis in consectetur, porro corporis placeat similique doloremque, nesciunt a delectus, illum nobis.
+        Provident totam explicabo sed nam sequi eius sint, nobis minima rem. Dicta, iste. Ut, magnam. Deserunt tenetur asperiores in, impedit veritatis quaerat consectetur aut ipsam ea adipisci mollitia architecto cumque!
+        Perferendis laborum at voluptatibus temporibus reiciendis maiores quos quae! Neque, temporibus eligendi omnis non, ea consequuntur dolorem totam iste blanditiis, veniam laborum perferendis itaque odit voluptatum delectus eum illum consectetur?
+        Aperiam provident sequi dicta id possimus iusto maxime laborum, impedit nulla voluptatibus porro quisquam unde odit voluptas explicabo magnam pariatur ab labore omnis veniam recusandae cumque illo saepe? Vel, adipisci.
+        Voluptates rem assumenda veritatis, quidem, dolor illum dolores sequi enim blanditiis illo minima hic distinctio excepturi doloremque iste? Optio cumque maiores modi pariatur repellendus rerum id quas ad at enim?
+        Cum sunt molestias modi magni quod, non impedit repudiandae doloribus ad? Repudiandae accusantium, dolore accusamus corrupti adipisci facere, sapiente eius et autem animi quibusdam omnis laborum amet aperiam dolor corporis.
+        Similique eum iusto ab tempore repellendus quis, error exercitationem, veritatis corrupti suscipit possimus velit eius voluptate aliquam unde et consequatur beatae, quia corporis nihil. Voluptatibus adipisci provident facilis laboriosam ipsa!
+        Dicta ex dolore dolor praesentium. Ex odit neque, quas dolore reprehenderit laborum temporibus placeat atque praesentium eligendi assumenda molestiae veniam minima aliquam illum autem eaque possimus enim voluptatibus natus. Consectetur.
+        Natus officiis optio iure deserunt, saepe quo illo delectus totam aspernatur itaque possimus sequi adipisci illum incidunt laboriosam. Corrupti recusandae unde nihil quia. Tempore aliquam totam vitae. Minima, sed est!
+        Voluptatum optio fuga nam quibusdam et. Itaque aspernatur quisquam, qui quam voluptas saepe sunt laboriosam vero incidunt. Consequatur aliquam velit maiores voluptatibus voluptate fugiat. A itaque quo est rerum rem?
+        Itaque animi amet quo eaque qui ea, consequuntur alias sapiente facilis tempora explicabo repellat temporibus debitis atque, neque nihil! In cupiditate consequuntur fugit velit inventore sed obcaecati ab labore pariatur!
+        Ad labore commodi sint voluptas fugiat facere consectetur quos inventore dolor assumenda. Cumque est iusto, porro eveniet corporis veniam ex omnis. Ea totam quae in iste est eius excepturi molestias!
+        Deserunt inventore, quasi temporibus nostrum et odit est rerum iste quis dolor quaerat repellat hic adipisci omnis laborum exercitationem vitae amet. Suscipit molestiae quod minima ipsum voluptatum a, non harum.
+        Distinctio voluptatem quidem aliquid autem, alias fugiat sunt dignissimos veniam eligendi? Voluptatibus, quidem assumenda, voluptatem error impedit natus eos, delectus unde tempora suscipit repellat eius nesciunt adipisci cumque perferendis quisquam.
+        Et eveniet labore fuga quisquam eum dignissimos optio suscipit omnis aliquam? Esse, enim itaque amet numquam laborum provident porro harum, qui, aperiam exercitationem ipsa illo voluptatum dolores? At, ad sed.
+        Veniam maxime nesciunt perferendis, exercitationem aliquid voluptatibus. Nam, quibusdam tenetur praesentium voluptates laboriosam, animi odio rerum reprehenderit nulla, accusantium dignissimos itaque sed corporis rem enim at ducimus ipsum libero perferendis.
+        Inventore illo facilis recusandae enim quos perspiciatis distinctio? Eius sapiente libero nostrum velit quod atque tempore pariatur nam molestias repellendus, illo nihil officiis. Quisquam ipsum et cupiditate! Expedita, nulla est!
+        Maiores esse, a officia tempora incidunt libero non nostrum ratione labore aut distinctio impedit minus sint vero corporis natus consectetur, nihil unde, id placeat? Tempore perspiciatis voluptates minus accusantium neque!
+        Consectetur atque harum iure asperiores quaerat inventore reprehenderit incidunt totam ipsa sed, rerum, vitae provident! Sequi excepturi nulla assumenda veritatis. Vitae quo excepturi perferendis illum aliquid atque ducimus libero earum.
+        Sit quibusdam accusantium quos dolores, voluptatibus earum hic, unde suscipit exercitationem cumque cum. Ipsam voluptas fugit, autem vero dignissimos eveniet quibusdam rerum, commodi voluptatum nisi, molestiae nihil deleniti nulla rem?
+        Eligendi enim consectetur nemo nostrum possimus rerum veniam quia explicabo voluptatibus expedita asperiores quisquam nesciunt doloremque suscipit unde odio corporis, tempore atque, fugit ipsam consequatur totam porro delectus natus. Nisi.
+        Amet delectus, esse, blanditiis ullam reprehenderit, nobis nihil molestiae quae est nostrum excepturi repellat cupiditate accusamus tempore officiis itaque commodi. Officiis facere doloremque omnis ipsam tenetur ea! Facere, dignissimos eum!
+        Illo similique voluptate corrupti. Dignissimos velit ea iure reiciendis aliquid dolore iste impedit, consectetur itaque dolor porro, mollitia earum nihil maiores reprehenderit incidunt libero eius quod error repudiandae? Sequi, reiciendis?
+        Iure accusamus repellat quod, quos vero ea aliquam, distinctio repellendus eveniet, corrupti officia maxime praesentium ipsam. Ad, ex voluptatibus! Aliquid aliquam voluptatibus corrupti error ducimus deleniti vitae quas quis placeat.
+        Deserunt, animi fuga a, nesciunt obcaecati quisquam, amet iure exercitationem est error corrupti nemo. Autem veritatis eligendi nemo minima qui id, error voluptatum distinctio quis repellat cupiditate dicta architecto corrupti?
+        Nam quidem earum quam iure dolorum amet ullam mollitia impedit ipsam aliquid qui, deserunt a reiciendis voluptatibus molestiae eum commodi placeat in porro doloremque ratione optio error. Non, minus quaerat?
+        Obcaecati accusantium sed eum molestiae explicabo velit hic perferendis. Magni ullam laboriosam totam obcaecati consectetur tenetur saepe voluptatum fuga aperiam nobis at natus ut, provident modi temporibus in nulla enim.
+        Qui quae expedita unde magnam error enim illum totam, ipsam ullam quia temporibus odio quibusdam praesentium vitae quod voluptatum sint beatae laudantium. Adipisci architecto unde esse quasi? Corrupti, non fuga!
+        Eaque error sit placeat distinctio molestiae quasi ut hic perferendis reprehenderit ad mollitia corrupti magni quas fuga commodi atque, delectus ea quod vero at repudiandae? Veniam expedita quis autem fugiat?
+        Rem inventore odit dicta voluptas voluptatibus doloribus accusamus alias consequatur quam voluptatum? Sint quis minima adipisci expedita quia enim voluptatibus, quos quo, officia vero tempore id reiciendis minus doloremque modi!
+        Ipsum sit in repellat officia totam harum quaerat omnis dolorum debitis eveniet, ratione excepturi repudiandae et quasi temporibus recusandae sint eius molestiae? Animi, deleniti rerum sed tempora ipsum vel! Cumque.
+        Dolorem, ut sint ipsa ullam in architecto odit totam soluta veniam et praesentium animi nemo perferendis obcaecati error voluptas expedita molestias eligendi culpa dicta enim numquam beatae natus. Commodi, nulla?
+        Laboriosam hic, odio nobis libero deleniti nam nesciunt incidunt illum minima, cum fuga officia consequuntur saepe mollitia ex porro quidem vel unde repellendus harum, placeat consectetur perspiciatis. Dolor, molestiae quidem.";
+        $newText = explode(".", $text);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php for($i=0; $i<count($newText); $i++) { ?>
+    <p>
+        <?php echo "{$newText[$i]}"; ?>
+    </p>
+    <?php } ?>
+        
+</body>
+</html>
